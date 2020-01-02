8ccce6c15d063d1870148f9b2a28c456b756bcc5
Gonçalo <goncalo.valentim@polarising.com>
2020-01-02 10:07:55 +0000

    Initial commit

---
+++ src/components/dashboard.jsx
@@ -0,0 +1,54 @@
+import React, {Component} from 'react';
+import logo from '../images/logo.svg'
+
+class Dashboard extends Component {
+    state = {
+        roles: []
+    };
+
+    render() {
+        return ();
+    //         <div className="wrapper">
+    //             <nav id="khq-sidebar">
+    //                 <div className="sidebar-header">
+    //                     <a href="#">
+    //                         <h3 className="logo">
+    //                             <img src={logo} alt=""/>
+    //                             <sup><strong>HQ</strong></sup>
+    //                         </h3>
+    //                     </a>
+    //                 </div>
+    //             </nav>
+    //             <ul className="list-unstyled components">
+    //                 <li class="${(tab == " cluster")?then(" active", "")}">
+    //                     <a href="#clusters"
+    //                        data-toggle="collapse"
+    //                        aria-expanded="false"
+    //                        class="dropdown-toggle">
+    //                         <i
+    //                             class="fa fa-fw fa fa-database"
+    //                             aria-hidden="true"
+    //                         />
+    //                         Clusters
+    //                         <span class="badge badge-success">
+    //                             ${clusterId}
+    //                         </span>
+    //                     </a>
+    //                     <ul class="collapse list-unstyled" id="clusters">
+    //                         <
+    //                         #list clusters as cluster>
+    //                         <li>
+    //                             <a href="${basePath}/${cluster}" class="${(cluster == clusterId)?then("
+    //                                active", "")}">${cluster}</a>
+    //                         </li>
+    //                     </
+    //                     #list>
+    //             </ul>
+    //         </li>
+    // </ul>
+    // </div>
+    // )
+    //     ;
+    // }
+
+export default Dashboard;
